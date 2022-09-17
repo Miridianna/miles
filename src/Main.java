5ebe248d71015c209adf7c2c75f1a26019843f45
Anna Yakovleva <akovlevaanna47@gmail.com>
2022-09-17 12:47:02 +0300

    add gitignore

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 1000;
+        int bonus = ticket / 20;
+        {
+            System.out.println(bonus);
+        }
+    }
+}
